2e78da6241816053d4f28c137de8bc0e8a5ecb95
Veronica <veronicawu95@hotmail.com>
2017-03-19 15:57:28 -0700

    adds audio to andy's page, fixes problems on safari mobile

--- js/on-scroll-to.js
+++ js/on-scroll-to.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
   var videoVivianOutro = document.getElementById("video-vivian-outro");
-
   var audioVivianWorry = document.getElementById("audio-vivian-worry");
   var audioVivianHuh = document.getElementById("audio-vivian-huh");
   var audioVivianAnnoying = document.getElementById("audio-vivian-annoying");
   var audioVivianContribute= document.getElementById("audio-vivian-contribute");
 
+  var videoAndyOutro = document.getElementById("video-andy-outro");
+  var audioAndyWelcoming= document.getElementById("audio-andy-welcoming");
+  var audioAndyDivide = document.getElementById("audio-andy-divide");
+  var audioAndyHalloween = document.getElementById("audio-andy-halloween");
+  var audioAndyInvited= document.getElementById("audio-andy-invited");
+  var audioAndyTemporary= document.getElementById("audio-andy-temporary");
+
   //mobile only
-  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  if (/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent) ) {
     var i = 0,
         f = function () {
             $('#assumptions-li-'+ i).addClass('animated');
@@ -17,18 +23,17 @@ $(document).ready(function() {
     f();
   }
   else { //desktop only
-    // This is then function used to detect if the element is scrolled into view
+    // This is the function used to detect if the element is scrolled into view
     $(window).scroll(function(){
-      function elementScrolled(elem)
+      function elementScrolled(elem, offsetTop)
       {
           var docViewTop = $(window).scrollTop();
           var docViewBottom = docViewTop + $(window).height();
-          var elemTop = $(elem).offset().top;
+          var elemTop = $(elem).offset().top + offsetTop;
           return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
       }
 
-      // This is where we use the function to detect if ".box2" is scrolled into view, and when it is add the class ".animated" to the <p> child element
-      if(elementScrolled('#assumptions')) {
+      if(elementScrolled('#assumptions', 300)) {
           var i = 0,
               f = function () {
                   $('#assumptions-li-'+ i).addClass('animated');
@@ -38,35 +43,74 @@ $(document).ready(function() {
           f();
       }
 
-      if(elementScrolled('#video-vivian-outro')) {
-        if (videoVivianOutro.played.length === 0 ) {
-          videoVivianOutro.play();
+      // vivian quotes
+      // if(elementScrolled('#video-vivian-outro')) {
+      //   if (videoVivianOutro.played.length === 0 ) {
+      //     videoVivianOutro.play();
+      //   }
+      // }
+      //
+      // if(elementScrolled('#quote-worry')) {
+      //   if (audioVivianWorry.played.length === 0 ) {
+      //     audioVivianWorry.play();
+      //   }
+      // }
+      //
+      // if(elementScrolled('#quote-huh')) {
+      //   if (audioVivianHuh.played.length === 0 ) {
+      //     audioVivianHuh.play();
+      //   }
+      // }
+      //
+      // if(elementScrolled('#quote-annoying')) {
+      //   if (audioVivianAnnoying.played.length === 0 ) {
+      //     audioVivianAnnoying.play();
+      //   }
+      // }
+      //
+      // if(elementScrolled('#quote-contribute')) {
+      //   if (audioVivianContribute.played.length === 0 ) {
+      //     audioVivianContribute.play();
+      //   }
+      // }
+
+      // andy quotes
+      if(elementScrolled('#video-andy-outro', 0)) {
+        if (videoAndyOutro.played.length === 0 ) {
+          videoAndyOutro.play();
         }
       }
 
-      if(elementScrolled('#quote-worry')) {
-        if (audioVivianWorry.played.length === 0 ) {
-          audioVivianWorry.play();
+      if(elementScrolled('#quote-welcoming', 0)) {
+        if (audioAndyWelcoming.played.length === 0 ) {
+          audioAndyWelcoming.play();
         }
       }
 
-      if(elementScrolled('#quote-huh')) {
-        if (audioVivianHuh.played.length === 0 ) {
-          audioVivianHuh.play();
+      if(elementScrolled('#quote-divide', 0)) {
+        if (audioAndyDivide.played.length === 0 ) {
+          audioAndyDivide.play();
         }
       }
 
-      if(elementScrolled('#quote-annoying')) {
-        if (audioVivianAnnoying.played.length === 0 ) {
-          audioVivianAnnoying.play();
+      if(elementScrolled('#quote-halloween', 0)) {
+        if (audioAndyHalloween.played.length === 0 ) {
+          audioAndyHalloween.play();
         }
       }
 
-      if(elementScrolled('#quote-contribute')) {
-        if (audioVivianContribute.played.length === 0 ) {
-          audioVivianContribute.play();
+      if(elementScrolled('#quote-invited', 0)) {
+        if (audioAndyInvited.played.length === 0 ) {
+          audioAndyInvited.play();
         }
       }
+
+      if(elementScrolled('#quote-temporary', 0)) {
+        if (audioAndyTemporary.played.length === 0 ) {
+          audioAndyTemporary.play();
+        }
+      }
+
     });
   }
 
--- js/video.js
+++ js/video.js
@@ -1,19 +1,23 @@
 var videoVivianIntro = document.getElementById("video-vivian-intro");
-var skipVideoLink = document.getElementById("skip-video-link");
+var skipAndyVideoLink = document.getElementById("skip-andy-video-link");
 var videoVivianOutro = document.getElementById("video-vivian-outro");
+
+var videoAndyIntro = document.getElementById("video-andy-intro");
+var videoAndyOutro = document.getElementById("video-andy-outro");
 
 var videoEnded = false;
 var scrolling = false;
 
 $(document).ready(function() {
-	videoVivianIntro.play();
-	
+	// videoVivianIntro.play();
+	videoAndyIntro.play();
+
 	$(window).on("mousewheel DOMMouseScroll", function() {
 		if ($('body').hasClass('noscroll')) {
 			if (videoEnded === false) {
 				if (scrolling === false) {
 					scrolling = true;
-					$('#skip-video-link').on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
+					$('#skip-andy-video-link').on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
 		     		$(this).removeClass("bounce");
 					}).addClass("bounce");
 					setTimeout(function() {scrolling = false}, 1500);
@@ -22,24 +26,30 @@ $(document).ready(function() {
 		}
 	});
 
-	$("#skip-video-link").click(function(event) {
+	$("#skip-andy-video-link").click(function(event) {
 		console.log("You clicked me!");
 		$('body').removeClass('noscroll');
-		videoVivianIntro.pause();
+		// videoVivianIntro.pause();
+		videoAndyIntro.pause();
 		$("#button-replay-intro").show();
 		$(".container-story-title").hide();
 	});
 
   $("#button-replay-intro").click(function(event) {
-		videoVivianIntro.currentTime = 0;
-		videoVivianIntro.play();
+		// videoVivianIntro.currentTime = 0;
+		// videoVivianIntro.play();
+
+		videoAndyIntro.currentTime = 0;
+		videoAndyIntro.play();
 		$("#button-replay-intro").hide();
 		$(".container-story-title").show();
   });
 
 	$("#button-replay-outro").click(function(event) {
-		videoVivianOutro.currentTime = 0;
-		videoVivianOutro.play();
+		// videoVivianOutro.currentTime = 0;
+		// videoVivianOutro.play();
+		videoAndyOutro.currentTime = 0;
+		videoAndyOutro.play();
 		$("#button-replay-outro").hide();
   });
 });
@@ -47,11 +57,12 @@ $(document).ready(function() {
 function startExperience() {
 	videoEnded = true;
 	console.log("video ended");
-	skipVideoLink.innerHTML = "View Vivian's experience";
-	$(skipVideoLink).removeClass("text-link").removeClass("skip-video-link-playing").addClass("button-default").addClass("skip-video-link-ended");
+	skipAndyVideoLink.innerHTML = "View Andy's experience";
+	$(skipAndyVideoLink).removeClass("text-link").removeClass("skip-video-link-playing").addClass("button-default").addClass("skip-video-link-ended");
 }
 
 function replayOutro() {
-	$("#button-replay-outro").show();
-	console.log("outro video's over");
+	if (!(/Mobi/.test(navigator.userAgent))) {
+		$("#button-replay-outro").show();
+	}
 }
